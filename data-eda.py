1ae521839c26ae51a6ca4c844d03b43045264260
IceBear9028 <tlatms9028@gmail.com>
2022-11-27 04:51:53 +0900

    3 commit
    1. data-eda 진행할 수 있는 주피터 파일 만들었음
    2. data별 eda 도 진행할 수 있는 Dash 파일 하나 만들었음

--- data-eda.py
+++ data-eda.py
@@ -1,25 +1,25 @@
-import plotly
 import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 import pandas as pd
 from flask import Flask
 
-df = pd.read_csv("./data/die-casting_dataset.csv")
+df = pd.read_csv('./data_refine/refine-error-lot-die-casting-dataset.csv', index_col=0)
+# df = pd.read_csv('./data_refine/refine-lot-die-casting-dataset.csv', index_col=0)
 
 server = Flask(__name__)
 app = Dash(__name__, server = server)
 
 column_name = df.columns
 
-error_data_column = df.columns[10 : ]
-condition_column = df.columns[1: 10]
-
+error_data_column = df.columns[13 : ]
+condition_column = df.columns[4: 13]
+lot_column = df.columns[:4]
 
 scatter_graph = 'scatter_graph'
 scatter_dropcontainer = 'scatter_dropdown_container'
-scatter_drop1 = 'scatter_drop1'
-scatter_drop2 = 'scatter_drop2' 
-
+scatter_drop1 = 'error_item_dropdown'
+scatter_drop2 = 'condition_dropdown'
+scatter_drop3 = 'LOT_x_value_dropdown'
 
 
 app.layout = html.Div(
@@ -31,28 +31,51 @@ app.layout = html.Div(
         html.Div(
             id = scatter_dropcontainer,
             children = [
-                dcc.Dropdown(
-                    id = scatter_drop1,
-                    options = [{'label' : item, 'value' : item} for item in error_data_column]
+                html.Div(
+                    id = 'error_item_container',
+                    children = [
+                        html.H3('에러조건 여부 선택'),
+                        dcc.Dropdown(
+                            id = scatter_drop1,
+                            options = [{'label' : item, 'value' : item} for item in error_data_column]
+                        ),
+                    ]
                 ),
-                dcc.Dropdown(
-                    id = scatter_drop2,
-                    options = [{'label' : item, 'value' : item} for item in condition_column]
-                )
+                html.Div(
+                    id = 'LOT_container',
+                    children = [
+                        html.H3('생산 분류 조건 선택 [x-좌표]'),
+                        dcc.Dropdown(
+                            id = scatter_drop3,
+                            options = [{'label' : item, 'value' : item} for item in lot_column]
+                        ),
+                    ]
+                ),
+                html.Div(
+                    id = 'condition_container',
+                    children = [
+                        html.H3('공정 조건 선택 [y-좌표]'),
+                        dcc.Dropdown(
+                            id = scatter_drop2,
+                            options = [{'label' : item, 'value' : item} for item in condition_column]
+                        ),
+                    ]
+                ),
             ]
         ),
     ]
 )
 
+
 @app.callback(
     Output(scatter_graph, 'figure'),
     Input(scatter_drop1, 'value'),
-    Input(scatter_drop2, 'value')
+    Input(scatter_drop2, 'value'),
+    Input(scatter_drop3, 'value'),
 )
-def scatterPlot(drop1 , drop2):
-    fig = px.scatter(df, x = drop1, y = drop2)
+def scatterPlot(error_item, condition, lot_condition):
+    fig = px.scatter(df, x = lot_condition, y = condition, color = error_item)
     return fig
-
 
 
 if __name__ == '__main__':
